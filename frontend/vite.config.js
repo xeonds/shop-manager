de5275b3203f6a5bfbe6a1368dd1506e6b31b432
xeonds <1456506329@qq.com>
2023-07-04 01:56:28 +0800

    Use auto import in frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,23 @@
 import { defineConfig } from "vite";
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
 import vue from "@vitejs/plugin-vue";
 import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    AutoImport({
+      imports: ["vue"],
+      dts: "src/auto-imports.d.ts",
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "/src"),
